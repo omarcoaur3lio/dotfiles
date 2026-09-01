43816de6726ed60336cf3c7e853263b899cfb2d9
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2026-08-31 23:31:36 -0300

    feat: update nvim config to set thin bar on indent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,13 +8,8 @@ vim.pack.add { gh 'omarcoaur3lio/roteki-theme.nvim' }
 
 -- Shortcut to reload the theme during development
 vim.keymap.set('n', '<leader>rr', function()
-  for name, _ in pairs(package.loaded) do
-    if name:match('^roteki') then
-      package.loaded[name] = nil
-    end
-  end
-  vim.cmd.colorscheme 'roteki'
-  vim.notify('Tema Roteki recarregado!', vim.log.levels.INFO)
+  require('roteki.utils').reload()
+  vim.notify('Tema Roteki recarregado (com cache limpo)!', vim.log.levels.INFO)
 end, { desc = '[R]eload [R]oteki Theme' })
 
 -- Koda Theme
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,14 @@ local gh = require('pack').gh
 
 -- [[ Visual indentation guides ]]
 vim.pack.add { gh 'lukas-reineke/indent-blankline.nvim' }
-require('ibl').setup {}
+require('ibl').setup {
+  indent = {
+    char = "▏",
+  },
+  scope = {
+    char = "▏",
+    show_start = false,
+  },
+}
 
 -- vim: ts=2 sts=2 sw=2 et
